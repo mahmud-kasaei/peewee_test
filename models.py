04955458f2a5ccc8f7354cb44c311014d090a4eb
mahmud.kasaei <mahmud.kasaei69@gmail.com>
2015-02-14 08:25:01 +0330

    UI , CR AND Relation Between Restaurant, Employe, job, category_job Complete..

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import peewee
+
 rest_db = peewee.MySQLDatabase('test_peewee', host="localhost", user="root", port=3306, passwd="")
 
 class MyRestModel(peewee.Model):
@@ -11,16 +12,34 @@ class Resturant(MyRestModel):
     name=peewee.CharField()
     address=peewee.TextField()
 
+    def employe_restaurant(self):
+        return (
+            Employe.select()
+            .join(Employe_Restaurant, on=Employe_Restaurant.employe)
+            .where(Employe_Restaurant.restaurant == self)
+        )
+
+class Category_Job(MyRestModel):
+    id=peewee.PrimaryKeyField()
+    name=peewee.TextField()
+
+
 class Job(MyRestModel):
     id=peewee.PrimaryKeyField()
     name=peewee.TextField()
+    category=peewee.ForeignKeyField(Category_Job, related_name="jobs", on_update="CASCADE", on_delete="RESTRICT")
 
 class Employe(MyRestModel):
     id=peewee.PrimaryKeyField()
     f_name=peewee.TextField()
     l_name=peewee.TextField()
-    job=peewee.ForeignKeyField(Job, related_name="jobs", on_delete="RESTRICT", on_update="CASCADE")
-    resturant=peewee.ForeignKeyField(Resturant,related_name="restaurants", on_delete="RESTRICT", on_update="CASCADE")
+    category_job=peewee.ForeignKeyField(Category_Job, related_name="employes_category_job", on_delete="RESTRICT", on_update="CASCADE")
+    job=peewee.ForeignKeyField(Job, related_name="employes_jobs", on_delete="RESTRICT", on_update="CASCADE")
+    # restaurant=peewee.ForeignKeyField(Resturant, related_name="employes_restaurants", on_delete="RESTRICT", on_update="CASCADE")
+    def rest_employe(self):
+        return (Employe.select()
+        .join(Employe_Restaurant, on=Employe_Restaurant.restaurant)
+        .where(Employe_Restaurant.employe=="self"))
 
 class Peripherals(MyRestModel):
     id=peewee.PrimaryKeyField()
@@ -35,12 +54,24 @@ class Room(MyRestModel):
     restaurant=peewee.ForeignKeyField(Resturant, related_name="rooms",on_update="CASCADE", on_delete="RESTRICT")
     emoloye=peewee.ForeignKeyField(Employe, related_name="rooms", on_update="CASCADE", on_delete="RESTRICT")
 
-class Category_Job(MyRestModel):
-    id=peewee.PrimaryKeyField()
-    name=peewee.TextField()
-    job=peewee.ForeignKeyField(Job, related_name="categories", on_update="CASCADE", on_delete="RESTRICT")
+class Employe_Restaurant(MyRestModel):
+    id = peewee.PrimaryKeyField()
+    employe = peewee.ForeignKeyField(Employe, related_name="employes_id", on_update="CASCADE", on_delete="RESTRICT")
+    restaurant = peewee.ForeignKeyField(Resturant, related_name="restaurants_id", on_update="CASCADE", on_delete="RESTRICT")
+
+    class Meta:
+        indexes = (
+            # Specify a unique multi-column index on from/to-user.
+            (('employe', 'restaurant'), True),
+        )
+
 
 rest_db.connect()
 
+
+
 if __name__ == "__main__":
-    rest_db.create_tables([Resturant, Employe, Peripherals, Room, Job, Category_Job])
+    # my.create_table()
+    rest_db.create_tables([Resturant, Employe, Peripherals, Room, Category_Job, Job, Employe_Restaurant])
+    # rest_db.create_table(Category_Job)
+    # rest_db.create_tables([Employe_Restaurant, Employe])
--- urls.py
+++ urls.py
@@ -1,7 +1,13 @@
 from handlers.restaurant__handler import CreateRestaurant, Retrive_Restaurant
+from handlers.category_job__handler import CreateCategoryJob
+from handlers.job__handler import CreateJob
+from handlers.employ__handler import CreateEmploye
 
 urlList  = [
 
-    (r'/', Retrive_Restaurant),
-    (r'/restaurant/add',CreateRestaurant )
+    (r'/restaurant$', Retrive_Restaurant),
+    (r'/restaurant/create$', CreateRestaurant),
+    (r'/category/create$', CreateCategoryJob),
+    (r'/job/create$', CreateJob),
+    (r'/employe/create$', CreateEmploye),
 ]
